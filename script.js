e39574a70b9d7ba98a9732009f7ac8bf8a152894
Jonathan Garuti <jonathan_garuti@hotmail.com>
2023-12-27 17:09:28 -0300

    Animação do Scroll

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function scrollSuave () {
   });
 }
 scrollSuave()
+
+function animacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        if(sectionTop < 0) {
+          section.classList.add('ativo');
+        }
+      })
+    }
+
+    animaScroll()
+
+    window.addEventListener('scroll', animaScroll)
+  }
+}
+
+animacaoScroll()
